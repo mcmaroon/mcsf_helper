aa3901d380ae40720d5b80ef65edb580786bf3c2
Marek Ciarkowski <marek.ciarkowski@gmail.com>
2018-12-01 23:12:48 +0100

    up

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MCSF\HelperBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('mcsf_helper');
+
+        return $treeBuilder;
+    }
+}
